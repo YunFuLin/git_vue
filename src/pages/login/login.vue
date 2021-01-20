f2d8a80caa29ddcb344a1ef3ea237d1e5ec5fc20
YunFuLin <1826656997@qq.com>
2021-01-20 09:17:27 +0800

    first commit

---
+++ src/pages/login/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <div class="headimg"></div>
+    <div class="inputbox">
+      <input type="text" placeholder="用户名" >
+      <input type="password" placeholder="密码">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username:'admin',
+      password:'123456'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .headimg{
+    width: 80px;
+    height: 80px;
+    background-color: bisque;
+    margin: 120px auto 50px;
+  }
+</style>
