7b0b33ec1bf625a030ceed66f309f5cc6abad0cd
YunFuLin <1826656997@qq.com>
2021-01-21 11:18:34 +0800

    插入less

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -2,9 +2,10 @@
   <div class="login">
     <div class="headimg"></div>
     <div class="inputbox">
-      <input type="text" placeholder="用户名" v-model="username">
-      <input type="password" placeholder="密码" v-model="password">
+      <input class="box-input" type="text" placeholder="用户名" v-model="username">
+      <input class="box-input" type="password" placeholder="密码" v-model="password">
     </div>
+    <div class="login-btn" @click="login">登&nbsp;&nbsp;&nbsp;&nbsp;录</div>
   </div>
 </template>
 
@@ -14,18 +15,46 @@ export default {
     return {
       username:'admin',
       password:'123456',
-      me:'123',
-      pc:'fit'
+    }
+  },
+  methods:{
+    login(){
+      console.log('用户名:'+this.username,'密码:'+this.password);
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
   .headimg{
     width: 80px;
     height: 80px;
     background-color: bisque;
-    margin: 120px auto 50px;
+    margin: 120px auto 10px;
+  }
+  .inputbox{
+    margin: 40px auto;
+    width: 80%;
+    .box-input{
+      display: block;
+      width: 100%;
+      height: 40px;
+      margin-bottom: 20px;
+      box-sizing: border-box;
+      border: 1px solid #999;
+      border-radius: 5px;
+      padding: 5px 10px;
+      }
+    }
+  .login-btn{
+    width: 80%;
+    margin: 0 auto;
+    height: 40px;
+    background: #33cc99;
+    font-size: 14px;
+    color: #fff;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 5px;
   }
 </style>
